3796707c2766efe93fdf69e9e44c5eb932d1a778
guul <thewalkingdoge@protonmail.com>
2017-12-24 16:58:36 +0000

    Update my_module.py

--- my_module.py
+++ my_module.py
@@ -1,7 +1,25 @@
 def write_to_file(name, data, encoding):
-    text_file = open(name, "w")
+	try:
+    	text_file = open(name, "w")
+	except IOError as error:
+		print("[!!] Error opening file " + str(error))
+		return -1
+	
     if enconding == 'utf-8':
-        text_file.write(data.encode('utf-8'))
+		try:
+        	text_file.write(data.encode('utf-8'))
+		except IOError as error:
+			print("[!!] Error writing to file " + str(error))
+			return -1
+			
     elif encoding == 'string':
-        text_file.write(str(data))
-    text_file.close()
+		try:
+        	text_file.write(str(data))
+		except IOError as error:
+			print("[!!] Error writing to file " + str(error))
+			return -1
+    
+	text_file.close()
+
+	
+	
